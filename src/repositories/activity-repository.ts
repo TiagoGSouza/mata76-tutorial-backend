2fbfb7d136736cf31715c786e33ae47af5c6aa1d
Tiago Gordiano de Souza <tiagogordiano@gmail.com>
2023-10-03 23:17:09 -0300

    add find functions

--- src/repositories/activity-repository.ts
+++ src/repositories/activity-repository.ts
@@ -1,3 +1,7 @@
+import { Activity } from "src/activities/entities/activity.entity";
+
 export abstract class ActivityRepository {
     abstract create(user: string, date: string, startTime: number, endTime: number, status: string): Promise<void>;
+    abstract findByDate(date: any) : Promise<Activity[]>;
+    abstract findAll(): Promise<Activity[]>;
 }
